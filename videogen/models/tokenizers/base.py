80637a74e9d4a70f59e39be2e69713a55dc27d2f
Jordan Lazzaro <jordantlazzaro@gmail.com>
2024-07-21 15:37:42 -0500

    adding base tokenizer class for refactor

---
+++ videogen/models/tokenizers/base.py
@@ -0,0 +1,41 @@
+from abc import ABC, abstractmethod
+from typing import Dict
+from torch import nn
+import torch
+
+
+class BaseTokenizer(nn.Module, ABC):
+    def __init__(self, config):
+        super().__init__()
+        self.config = config
+
+    def forward(self, x: torch.Tensor) -> Dict[str, torch.Tensor]:
+        pass
+
+    @abstractmethod
+    def encode(self, x: torch.Tensor) -> torch.Tensor:
+        pass
+
+    @abstractmethod
+    def decode(self, x: torch.Tensor) -> torch.Tensor:
+        pass
+
+    @abstractmethod
+    def discriminate(self, x: torch.Tensor) -> torch.Tensor:
+        pass
+
+    @abstractmethod
+    def reconstruction_loss(self, x_hat: torch.Tensor, x: torch.Tensor):
+        pass
+
+    @abstractmethod
+    def generator_loss(self, logits_fake: torch.Tensor):
+        pass
+
+    @abstractmethod
+    def discriminator_loss(self, logits_real: torch.Tensor, logits_fake: torch.Tensor):
+        pass
+
+    @abstractmethod
+    def gradient_penalty(self, x: torch.Tensor, logits_real: torch.Tensor):
+        pass
